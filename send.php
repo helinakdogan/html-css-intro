127e1901d04f1ef4d9969d8b29df9165defb176a
Helin Akdoğan <akdoganhelin@gmail.com>
2023-01-07 04:58:25 +0300

    .

---
+++ send.php
@@ -0,0 +1,8 @@
+<?php
+$userName = $_POST["userName"];
+$uName = $_POST["uName"];
+$userEmail = $_POST["userEmail"];
+echo "Your username:$userName<br>
+      Your name:$uName <br>
+      Your email address:$userEmail<br> ";
+?>
